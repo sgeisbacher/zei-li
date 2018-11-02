d7e1f46c04a7a677c3a40ca52642bed93150009a
Stefan Geisbacher <stefan.geisbacher@gmail.com>
2018-11-02 19:24:36 +0100

    introduced time-tracking start offset

--- README.md
+++ README.md
@@ -7,5 +7,6 @@ any use of these scripts and tools is at your own risk.
 ## usage
 ```
 zei list activities (<prefix>) # lists all your activities
+zei start <activity> (offset <time-offset>) # starts tracking an activity. optionally you could specify an offset in the form of e.g. 2h10m for 2hours and 10minutes
 ```
 
--- src/TimeTracking.hs
+++ src/TimeTracking.hs
@@ -1,6 +1,9 @@
 {-# LANGUAGE OverloadedStrings #-}
 module TimeTracking
 ( start
+, timeExprToSec
+, getOption
+, calculateStart
 ) where
 
 import Data.Aeson
@@ -12,26 +15,52 @@ import Data.Time.Format
 import qualified Data.ByteString.Lazy.Char8 as C8
 import qualified Data.ByteString.Lazy as B
 import Data.List.Utils
+import Text.Regex.Posix
+import Data.Maybe (fromMaybe)
 
 import qualified Activities
+
+-- (([0-9]+)h)?(([0-9]+)m)?(([0-9]+)s)?
 
 newtype TrackingStart = TrackingStart String
 instance ToJSON TrackingStart where
     toJSON (TrackingStart timestamp) = object
         [ "startedAt" .= timestamp ]
 
-currTimeStamp :: IO String
-currTimeStamp = take 23 . formatTime defaultTimeLocale "%Y-%m-%dT%H:%M:%S%Q" <$> getCurrentTime
+getOption :: String -> [String] -> Maybe String
+getOption "" _ = Nothing
+getOption _ [] = Nothing
+getOption _ [a] = Nothing
+getOption option opts 
+    | option `elem` opts = getOptionValueSafely . take 2 . dropWhile (/= option) $ opts
+    | otherwise = Nothing
+    where getOptionValueSafely [o] = Nothing
+          getOptionValueSafely (_:value:_) = Just value
+
+timeExprToSec :: String -> Integer
+timeExprToSec "" = 0
+timeExprToSec expr = do 
+    let matches = map (\x -> if x == "" then "0" else x ) . getAllTextSubmatches $ expr =~ ("(([0-9]+)h)?(([0-9]+)m)?(([0-9]+)s)?" :: String) :: [String]
+    let hours = read (matches!!2) :: Integer
+    let minutes = read (matches!!4) :: Integer
+    let seconds = read (matches!!6) :: Integer
+    hours * 3600 + minutes * 60 + seconds
+
+calculateStart :: UTCTime -> Integer -> String
+calculateStart currTime offset = do
+    let timeStamp = addUTCTime (realToFrac $ (-1) * offset) currTime
+    take 23 . formatTime defaultTimeLocale "%Y-%m-%dT%H:%M:%S.%q" $ timeStamp
 
 start :: Ctx -> [String] -> IO String
 start _ [] = return "activity required!"
-start ctx (activityName:_) = do
+start ctx (activityName:args) = do
     act <- Activities.findByName ctx activityName
     case act of 
         Nothing -> return "Error while starting activity (not found)"
         Just activity -> do
             let endpoint = replace "{activityId}" (Activities.id activity) C.endpointTimeTrackingStart
-            trackingStart <- TrackingStart <$> currTimeStamp
+            currTime <- getCurrentTime
+            let trackingStart = TrackingStart <$> calculateStart currTime $ timeExprToSec . fromMaybe "0s" $ getOption "offset" args 
             answer <- C8.unpack <$> Uplink.post (token ctx) endpoint trackingStart
             putStrLn $ "answer: " ++ answer 
             return $ "started " ++ Activities.name activity
